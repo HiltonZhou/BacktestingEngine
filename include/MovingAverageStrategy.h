80f05322d58b8a008a3da2a98fab924946de029b
Hilton <hiltonzhou6@gmail.com>
2026-07-05 17:10:29 -0700

    kk

--- include/MovingAverageStrategy.h
+++ include/MovingAverageStrategy.h
@@ -5,12 +5,12 @@
 class MovingAverageStrategy : public Strategy
 {
 private:
-    int amtCandle;
+    int amtCandle; //amount to look back on
 
     double calculateMovingAverage(const std::vector<Candle>& candles,int currentIndex) const;
 
 public:
-    MovingAverageStrategy(int amtCandle);
+    MovingAverageStrategy(int amtCandle); 
 
     Signal generateSignal(const std::vector<Candle>& candles,int currentIndex) const override;
 };
