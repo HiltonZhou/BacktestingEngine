5f9a1158f26f4bc6eb84abf51b1b0f749b5e2f65
Hilton <hiltonzhou6@gmail.com>
2026-07-07 16:42:16 -0700

    naming

---
+++ include/Indicators/EMA.h
@@ -0,0 +1,10 @@
+// Exponential Moving Avearage 
+
+#pragma once
+
+#include "SMA.h"
+
+class EMA : public SMA
+{
+    
+}
--- include/Indicators/SMA.h
+++ include/Indicators/SMA.h
@@ -1,3 +1,5 @@
+// Simple Moving Average
+
 #pragma once
 
 #include "Strategy.h"
